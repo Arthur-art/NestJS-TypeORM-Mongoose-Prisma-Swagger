3b863a13d2dff0c1f4bfe4ad95d7575e72c5ebbd
arthur-art <arthurteixeira.guts@gmail.com>
2022-04-25 13:08:29 -0300

    Trabalhando com parametros dinamicos no nestjs

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,15 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
     @Get('list')
     findAll(){
         return 'Listagem de cursos'
+    }
+
+    //Trabalhando com parametros courses/:id
+    @Get(':id')
+    findOne(@Param('id') id:string){
+        return `Curso #${id} - Trabalhando com parametros no nestjs`
     }
 }
