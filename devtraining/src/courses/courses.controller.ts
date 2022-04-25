6e2f385ad0bb5ee4147526a90bb604802ccfc5f4
arthur-art <arthurteixeira.guts@gmail.com>
2022-04-25 15:26:05 -0300

    Trabalhando com metodo Post

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,15 +1,27 @@
-import { Controller,Get, Param } from '@nestjs/common';
+import { Body, Controller,Get, Param, Post } from '@nestjs/common';
+
+type CreateUserProps = {
+    name: string;
+    description:string;
+    price: number;
+}
 
 @Controller('courses')
 export class CoursesController {
     @Get('list')
     findAll(){
-        return 'Listagem de cursos'
+        return 'Listagem de cursos';
     }
 
     //Trabalhando com parametros courses/:id
     @Get(':id')
     findOne(@Param('id') id:string){
-        return `Curso #${id} - Trabalhando com parametros no nestjs`
+        return `Curso #${id} - Trabalhando com parametros no nestjs`;
+    }
+
+    //Trabalhando com metodo Post
+    @Post()
+    createUser(@Body() body:CreateUserProps){
+        return body.description;
     }
 }
